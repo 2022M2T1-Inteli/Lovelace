926d86b98465bae44f2c861f69c874040797990c
lyorrei-inteli <Lyorrei.Quintao@sou.inteli.edu.br>
2022-05-23 14:45:45 -0300

    Adicionei redirecionamento na rota /

--- src/Backend/index.js
+++ src/Backend/index.js
@@ -5,6 +5,10 @@ const path = require('path')
 
 app.use(express.static(path.join(__dirname + '/../Frontend')));
 
+app.get('/', (req, res) => {
+    res.redirect('/views/landingPage/landingPage.html')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port http://localhost:${port}`)
 })
