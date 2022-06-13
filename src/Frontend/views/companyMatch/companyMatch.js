482b58f07f6fa0a3d1ce1d2b1d3205cfe84a2453
FelipeCampos14 <99193547+FelipeCampos14@users.noreply.github.com>
2022-06-13 15:20:31 -0300

    Tarefa companyMatch - mensagem sem empresa

--- src/Frontend/views/companyMatch/companyMatch.js
+++ src/Frontend/views/companyMatch/companyMatch.js
@@ -5,24 +5,29 @@ $(document).ready(() => {
         contentType: 'application/json',
         success: function (res) {
             let elements = ''
-            for (company of res) {
-                elements += `<div class="gridBox">
-                                <h4 class="boxTitle">${company.name}</h4>
-                                <p class="boxSubTitle">${company.city} / ${company.state}</p>
-                                <div class="badgeContainer">
-                                    <span class="badge badge-yellow">${company.marketNiche}</span>
-                                </div>
-                                <p class="boxText">
-                                    ${
-                                        company.companyPhilosophy.length > 160
-                                            ? company.companyPhilosophy.slice(0, 160)
-                                            : company.companyPhilosophy
-                                    }...
-                                </p>
-                                <a class="btn btn-yellow" href="/views/companyProfile/companyProfile.html?id=${
-                                    company.id
-                                }">Mais detalhes</a>
-                            </div>`
+            if (res.length == 0) {
+                elements = "<a class='errorMessage'>Nennhuma empresa encontrada.<br> Ainda não temos vagas que condizem com seu perfil!</a>"
+            }
+            else {
+                for (company of res) {
+                    elements += `<div class="gridBox">
+                                    <h4 class="boxTitle">${company.name}</h4>
+                                    <p class="boxSubTitle">${company.city} / ${company.state}</p>
+                                    <div class="badgeContainer">
+                                        <span class="badge badge-yellow">${company.marketNiche}</span>
+                                    </div>
+                                    <p class="boxText">
+                                        ${
+                                            company.companyPhilosophy.length > 160
+                                                ? company.companyPhilosophy.slice(0, 160)
+                                                : company.companyPhilosophy
+                                        }...
+                                    </p>
+                                    <a class="btn btn-yellow" href="/views/companyProfile/companyProfile.html?id=${
+                                        company.id
+                                    }">Mais detalhes</a>
+                                </div>`
+                }    
             }
             $('.grid').html(elements)
         },
