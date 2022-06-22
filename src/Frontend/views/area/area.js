80f1cb0c0990e8183ad457381bdad7bfada4b397
lyorrei-inteli <Lyorrei.Quintao@sou.inteli.edu.br>
2022-06-22 09:59:09 -0300

    Comentei area.js

--- src/Frontend/views/area/area.js
+++ src/Frontend/views/area/area.js
@@ -1,12 +1,16 @@
+// FUNÇÃO EXECUTADA QUANDO A PÁGINA É CARREGADA
 $(document).ready(() => {
+    // REFERENCIAR OS ELEMENTOS HTML
     const backdrop = $('.backdrop')
     const modal = $('.modal')
 
+    // AJAX PARA PEGAR AS ÁREAS QUE EXISTEM NO BANCO DE DADOS
     $.ajax({
         url: '/area',
         type: 'GET',
         contentType: 'application/json',
         success: function (res) {
+            // PREENCHER TABELA COM AS ÁREAS
             let areaElements = ''
             for (area of res) {
                 areaElements += `<tr class="row">
@@ -22,33 +26,44 @@ $(document).ready(() => {
         },
     })
 
+    // FUNÇÃO PARA ABRIR A MODAL
     const openModal = () => {
         backdrop.css('display', 'block')
         modal.css('display', 'block')
     }
 
+    // FUNÇÃO PARA FECHAR A MODAL
     const closeModal = () => {
         backdrop.css('display', 'none')
         modal.css('display', 'none')
     }
 
+    // ATRIBUIR A FUNÇÃO A CLIQUES EM ELEMENTOS HTML
     $('#modalBtn').click(openModal)
     $('.backdrop').click(closeModal)
 
+    // ESCUTAR CLIQUES NO ELEMENTO COM ID createArea
     $('#createArea').click(() => {
+        // PEGAR VALOR DO INPUT
         const areaName = $('#areaName').val()
 
+        // CHECAR SE VALOR DO INPUT EXISTE
         if (areaName) {
+            // AJAX PARA CRIAR NOVA ÁREA
             $.ajax({
                 url: '/area/create',
                 type: 'POST',
                 contentType: 'application/json',
                 data: JSON.stringify({ name: areaName }),
                 success: function (res) {
+                    // RECARREGAR A PÁGINA
                     window.location.reload()
                 },
                 error: function (err) {
+                    // FECHAR A MODAL
                     closeModal()
+
+                    // MOSTRAR ALERTA COM O ERRO
                     Swal.fire({
                         icon: 'error',
                         title: 'Oops...',
@@ -60,16 +75,19 @@ $(document).ready(() => {
     })
 })
 
+// FUNÇÃO DE DELETAR ÁREA
 function deleteArea(id) {
+    // REQUISIÇÃO PARA DELETAR ÁREA
     $.ajax({
         url: '/area/' + id,
         type: 'DELETE',
         contentType: 'application/json',
-
         success: function (res) {
+            // RECARREGAR PÁGINA
             window.location.reload()
         },
         error: function (err) {
+            // MOSTRAR ALERTA DE ERRO
             Swal.fire({
                 icon: 'error',
                 title: 'Oops...',
